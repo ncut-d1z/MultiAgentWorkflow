acd8d30615b09dfa8014c096bbc403c389bb6e2a
ncut-d1z <dong1z@mail.ncut.edu.cn>
2026-09-25 22:59:37 +0800

    Add src/multiagent_workflow/__init__.py

---
+++ src/multiagent_workflow/__init__.py
@@ -0,0 +1,6 @@
+"""MultiAgentWorkflow package."""
+
+from .orchestrator import WorkflowConfig, WorkflowResult, run_workflow
+
+__all__ = ["WorkflowConfig", "WorkflowResult", "run_workflow"]
+__version__ = "0.1.0"
